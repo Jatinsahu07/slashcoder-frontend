e1d04cbe7c6d15027b2982b8118bf3484ee06546
Jatinsahu07 <jatinsahu718@gmail.com>
2025-11-15 01:08:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// 🔥 Initialize socket at app bootstrap (important)
+import "./socket.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
